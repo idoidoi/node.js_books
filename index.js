e859b5463c33401dd6819626d2bdeb59cddb09d7
lee <dnr2608@naver.com>
2022-11-30 19:27:57 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var booksRouter = require('./books.js');
+ var router = require('./router.js');
+
+app.use(express.json());
+app.use(express.static('public'))
+app.use('/router',router);
+app.use('/books',booksRouter);
+
+
+app.listen(3000,function(){
+    console.log("3000포트로 웹서버 실행!");
+})
